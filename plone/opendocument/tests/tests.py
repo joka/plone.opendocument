9e816e4ec0aba00cb1e1a2a6e10114a3d1c39985
joka <joka@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-08-20 16:37:51 +0000

    rename

--- plone/opendocument/tests/tests.py
+++ plone/opendocument/tests/tests.py
@@ -5,16 +5,16 @@ from zope.testing import doctest
 from zope.testing.doctestunit import DocFileSuite
 
 from plone.transforms.tests.utils import configurationSetUp
-from plone.opendocument import opendocument_to_xhtml, utils
+from plone.opendocument import opendocument_html_xslt, utils
 
 
 optionflags = doctest.REPORT_ONLY_FIRST_FAILURE | doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
 
 def test_suite():
    global optionsflags
-   if opendocument_to_xhtml.HAS_LXML and utils.HAS_PIL:
+   if opendocument_html_xslt.HAS_LXML and utils.HAS_PIL:
         return unittest.TestSuite(
-            [doctest.DocFileSuite('opendocument_to_xhtml.text',
+            [doctest.DocFileSuite('opendocument_html_xslt.text',
                                  package='plone.opendocument', 
                                  optionflags=optionflags,
                                  tearDown=tearDown,
